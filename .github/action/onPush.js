1b6bbf9a88c6df51d1fd058eeabc03e72f380a1f
Matteo Tamine <matteo.tamine@epitech.eu>
2022-07-13 14:44:13 +0200

    Moving action here

---
+++ .github/action/onPush.js
@@ -0,0 +1,34 @@
+const { Client } = require("@notionhq/client")
+require('dotenv').config()
+
+const notion = new Client({ auth: process.env.NOTION_KEY })
+
+const databaseId = process.env.TEAM_TASK_NOTION_DB_ID
+
+const updateNotion = async () => {
+  const GITHUB_BRANCH = process.env.GITHUB_REF
+  const GITHUB_BRANCH_SPLITTED = GITHUB_BRANCH.split('/')
+  const GITHUB_BRANCH_NAME = GITHUB_BRANCH_SPLITTED[GITHUB_BRANCH_SPLITTED.length - 1]
+  const getId = branchName => {
+    const id = branchName.split('-')
+    let result = ''
+    for (let i = 0; i < 2; i++)
+      result += id[i] + '.'
+    result += id[2]
+    return result
+  }
+  const resp = await notion.databases.query({ database_id: databaseId })
+  const pages = resp.results.filter(item => item.properties.Name.title[0]?.text.content.startsWith(getId(GITHUB_BRANCH_NAME)))
+  pages.forEach(async page => {
+    const pageId = page.id
+    if (page.properties.Status.select.name == 'To Do')
+      await notion.pages.update({ page_id: pageId, properties: {
+        'Status': {
+          select: {
+            name: 'In Progress',
+          },
+        },
+      }})
+  })
+}
+updateNotion()
